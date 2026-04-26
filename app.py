f55435fe1b898bf0e1dee47cc7d27eb161fc042a
ruchitawagh-06 <ruchitawagh06@gmail.com>
2026-04-26 23:24:20 +0530

    app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import os
 
 app = Flask(__name__)
 app.secret_key = "1234"
-
-API_KEY = os.environ.get("54ff43f30c3097a69d22d7f5ff91701f")  # 🔐 safer for deployment
+API_KEY = os.environ.get("API_KEY") # 🔐 safer for deployment
 
 
 # 🤖 AI Suggestion
